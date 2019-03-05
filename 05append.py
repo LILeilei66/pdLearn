22af9caa81125fd23c66a6d8852576c0741243cd
LILeilei <40779351+LILeilei66@users.noreply.github.com>
2019-03-05 11:02:52 +0800

    classification with sklearn

--- 05append.py
+++ 05append.py
@@ -1,4 +1,6 @@
 """
+只能 append 同类型, 如: pd.Series 或是 pd.DataFrame
+
 append 多维, 需要指名这多维的 list 是在同一个 index 中的, 否则会建立多个index.
 ex.:
 item = pd.DataFrame(data=[], columns=columns)
@@ -6,20 +8,37 @@ item.loc[0] = value
 print(item)
                         ft1                       ft2
 0  [[123, 234], [156, 314]]  [[523, 635], [261, 543]]
+df2 = df.append(item, ignore_index=True)
+
 """
 
 # https://github.com/hangsz/pandas-tutorial/blob/master/2.%20Series%E5%92%8CDataFrame%E5%AF%B9%E8%B1%A1%E7%9A%84%E6%9F%A5%E3%80%81%E6%94%B9%E3%80%81%E5%A2%9E%E3%80%81%E5%88%A0.ipynb
 
 import pandas as pd
+from numpy import array as nparray
 
 # ================================================================
-# 1. Series
+# 0. Series 数值 (label)
+# ================================================================
+s = pd.Series(data=[])
+for i in range(3):
+    item = pd.Series(data=i)
+    s = s.append(item, ignore_index=True)
+print(s)
+"""
+0    0
+1    1
+2    2
+dtype: int64
+"""
+# ================================================================
+# 1. Series 多维
 # ================================================================
 data = [[[1,2],[3,4]],
         [[3,4],[1,7]],
         [[4,7],[2,9]]]
 s = pd.Series(data=data)
-# print(s)
+print(s)
 """
 0    [[1, 2], [3, 4]]
 1    [[3, 4], [1, 7]]
@@ -27,7 +46,7 @@ s = pd.Series(data=data)
 dtype: object"""
 item = pd.Series([[[123,234],[456,862]]])
 s2 = s.append(item, ignore_index=True)
-# print(s2)
+print(s2)
 """
 0            [[1, 2], [3, 4]]
 1            [[3, 4], [1, 7]]
@@ -41,9 +60,10 @@ dtype: object"""
 # ================================================================
 columns = ['ft1', 'ft2']
 df = pd.DataFrame(data=data, columns=columns)
-# print(df)
-value = [[[123,234],[156,314]],
-         [[523,635],[261,543]]]
+print(df)
+a = [[123,234],[156,314]]
+b = [[523,635],[261,543]]
+value = [a, b]
 item = pd.DataFrame(data=value)
 print(item)
 """  0           1
@@ -78,3 +98,5 @@ print(df2)
 1                    [3, 4]                    [1, 7]
 2                    [4, 7]                    [2, 9]
 3  [[123, 234], [156, 314]]  [[523, 635], [261, 543]]"""
+print('--------------')
+print(nparray(df2.loc[3, 'ft1']).shape)
---
+++ 06pd+clf.py
@@ -0,0 +1,111 @@
+"""
+df.astype(np.float64) 的适用情况:
+1.  df = DataFrame(data=[], columns=['ft1', 'ft2'])
+    value1 = nparray([1])
+    value2 = nparray([14])
+    value3 = nparray([1346])
+    value4 = nparray([17])
+
+2. df = DataFrame(data=[], columns=range(2))
+    将 features 全部拆开
+
+3. prediction:
+    reshape(1,-1) 以表现为一个sample.
+    ex: df.loc[0].values.reshape(1,-1)
+
+"""
+
+from pandas import DataFrame, Series
+from sklearn.ensemble import RandomForestClassifier as skrandomforest
+from numpy import array as nparray
+import numpy as np
+
+# ====================================================================
+# 1. 多特征, 每个特征为一个数 [Pass]
+# ====================================================================
+
+value1 = nparray([1])
+value2 = nparray([14])
+value3 = nparray([1346])
+value4 = nparray([17])
+
+df = DataFrame(data=[], columns=['ft1', 'ft2'])
+df.loc[0, 'ft1'] = value1
+df.loc[0, 'ft2'] = value2
+df.loc[1, 'ft1'] = value3
+df.loc[1, 'ft2'] = value4
+
+df.astype(np.float64)
+
+
+# ====================================================================
+# 2. 多特征, 每个特征为一个array [Fail]
+# ValueError: setting an array element with a sequence.
+# ====================================================================
+
+value1 = nparray([1,2])
+value2 = nparray([14,25])
+value3 = nparray([1346,1])
+value4 = nparray([17,156])
+
+df = DataFrame(data=[], columns=['ft1', 'ft2'])
+df.loc[0, 'ft1'] = value1
+df.loc[0, 'ft2'] = value2
+df.loc[1, 'ft1'] = value3
+df.loc[1, 'ft2'] = value4
+
+df.astype(np.float64)
+
+
+# ====================================================================
+# 3. 单特征, 每个特征为一个array [Fail]
+# ValueError: setting an array element with a sequence.
+# ====================================================================
+
+value1 = nparray([1,2])
+value2 = nparray([14,25])
+
+s = Series(data=[])
+s.loc[0] = value1
+s.loc[1] = value2
+
+s.astype(np.float64)
+
+# ====================================================================
+# 4. 将 features 全部拆开
+# ====================================================================
+value1 = nparray([1,2])
+value2 = nparray([14,25])
+df = DataFrame(data=[], columns=range(2))
+df.loc[0] = value1
+df.loc[1] = value2
+
+# print(df)
+"""
+    0   1
+0   1   2
+1  14  25
+"""
+
+print(type(df))
+label = [1,0]
+y = Series(data=label)
+
+
+clf = skrandomforest(n_estimators=1, max_depth=2)
+clf.fit(df, y)
+
+
+predict1 = clf.predict(df.loc[0].values.reshape(1,-1))
+predict2 = clf.predict(df.loc[1].values.reshape(1,-1))
+print('prediction: ', predict1, predict2)
+
+# print(clf)
+"""
+RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=2, max_features='auto', max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=1,
+            oob_score=False, random_state=None, verbose=0,
+            warm_start=False)"""
